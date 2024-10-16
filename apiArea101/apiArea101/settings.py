0bd726632ff4fe7ebf5c7258f345f1c43f787d5c
SergioLM7 <sergiolillom@gmail.com>
2024-10-16 20:19:50 +0200

    new port added to CORS

--- apiArea101/apiArea101/settings.py
+++ apiArea101/apiArea101/settings.py
@@ -136,6 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://0.0.0.0:10000",
     "https://front-nuxt-area101.vercel.app",
     "https://django-area101.onrender.com",
 ]
